e0ba0b81db42308943363811c766122b85b20ebd
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2017-11-06 09:29:10 -0500

    Add files via upload

---
+++ SPC3.cpp
@@ -0,0 +1,14 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+// [[Rcpp::export]]
+NumericVector SPC3(NumericVector y,
+                   NumericVector blk, NumericVector row, NumericVector col,
+                   int rN=2, int cN=2){
+  int n = y.size(); NumericVector Cov(n), Phe(n), Obs(n);
+  for(int i=0; i<n; i++){
+    // Searching for neighbors
+    for(int j=0; j<n; j++){
+        if( (i>j) & (blk[i]==blk[j]) & (abs(row[i]-row[j])<=rN) & (abs(col[i]-col[j])<=cN) ){
+          Phe[i] = Phe[i]+y[j]; Obs[i] = Obs[i]+1; Phe[j] = Phe[j]+y[i]; Obs[j] = Obs[j]+1; }}}
+  // Averaging the neighbors
+  Cov = Phe/Obs; return Cov;}
