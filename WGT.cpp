b6dad8ad2ebce11d29b21b385a2f981146eef742
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2018-07-16 08:08:51 -0500

    Delete WGT.cpp

--- WGT.cpp
+++
@@ -1,8 +0,0 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-// [[Rcpp::export]]
-void WGT(NumericMatrix X, NumericVector w){
-  int p = X.ncol(); int n = X.nrow();
-  for(int j=0; j<p; j++){
-    for(int i=0; i<n; i++){
-    X(i,j) = X(i,j)*w[i];};};}
