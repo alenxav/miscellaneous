d7dc29127f800bebe7783b32e84ebe9dce1e40d2
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2018-08-07 10:44:30 -0500

    Update WGR_emGREML.cpp

--- WGR_emGREML.cpp
+++ WGR_emGREML.cpp
@@ -1,18 +1,24 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 // [[Rcpp::export]]
-SEXP emREML(NumericVector y, NumericMatrix gen, int maxit = 500, double tol = 10e-6){
+SEXP emREML(NumericVector y, NumericMatrix X,
+           Rcpp::Nullable<Rcpp::NumericVector> D = R_NilValue,
+           int maxit = 500, double tol = 10e-8){
   // Functions starts here
-  int p = gen.ncol();
-  int n = gen.nrow();
+  int p = X.ncol();
+  int n = X.nrow();
+  // Weights
+  bool P_WEIGHTS = FALSE;
+  NumericVector d(p);
+  if (D.isNotNull()){P_WEIGHTS = TRUE; d=D;}
   // Beta, mu and epsilon
   double b0, eM, ve, vb, h2, mu = mean(y), vy = var(y);
   NumericVector b(p), e = y-mu;
   // Marker variance
   NumericVector xx(p), vx(p);
   for(int i=0; i<p; i++){
-    xx[i] = sum(gen(_,i)*gen(_,i));
-    vx[i] = var(gen(_,i));}
+    xx[i] = sum(X(_,i)*X(_,i));
+    vx[i] = var(X(_,i));}
   double MSx = sum(vx), Lmb=MSx;
   // Convergence control
   NumericVector bc(p);
@@ -24,8 +30,11 @@ SEXP emREML(NumericVector y, NumericMatrix gen, int maxit = 500, double tol = 10
     bc = b+0;
     for(int j=0; j<p; j++){
       b0 = b[j];
-      b[j] = (sum(gen(_,j)*e)+xx[j]*b0)/(xx[j]+Lmb);
-      e = e-gen(_,j)*(b[j]-b0);}
+      if(P_WEIGHTS){
+        b[j] = (sum(X(_,j)*e)+xx[j]*b0)/(xx[j]+Lmb/d[j]);
+          }else{
+        b[j] = (sum(X(_,j)*e)+xx[j]*b0)/(xx[j]+Lmb);}
+      e = e-X(_,j)*(b[j]-b0);}
     // Variance components update
     ve = sum(e*y)/(n-1);
     vb = (vy-ve)/MSx;
@@ -40,7 +49,7 @@ SEXP emREML(NumericVector y, NumericMatrix gen, int maxit = 500, double tol = 10
     if( cnv<tol ){break;}}
   // Fitting the model
   NumericVector fit(n);
-  for(int k=0; k<n; k++){ fit[k] = sum(gen(k,_)*b)+mu; }
+  for(int k=0; k<n; k++){ fit[k] = sum(X(k,_)*b)+mu; }
   h2 = vb*MSx/(vb*MSx+ve);
   // Output
   return List::create(Named("mu")=mu, Named("b")=b,
