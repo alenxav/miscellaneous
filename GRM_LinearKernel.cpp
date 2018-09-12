aaed3debe006a82cb4bb436a0ec928427428923c
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2018-09-12 12:08:57 -0500

    Update GRM_LinearKernel.cpp

--- GRM_LinearKernel.cpp
+++ GRM_LinearKernel.cpp
@@ -1,11 +1,20 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 // [[Rcpp::export]]
-NumericMatrix GRM(NumericMatrix X){
+NumericMatrix GRM(NumericMatrix X, bool Code012 = false){
   int n = X.nrow(), p = X.ncol();
-  NumericMatrix K(n,n); NumericVector xx(p); double zz;
-  for(int i=0; i<n; i++){; for(int j=0; j<n; j++){
-        zz = sum( (X(i,_)-xx(i))*(X(j,_)-xx(j)) );
-        K(i,j)=zz; K(j,i)=zz;}}
-  for(int i=0; i<p; i++){xx[i] = K(i,i);}
-  return K/mean(xx);}
+  NumericMatrix K(n,n); NumericVector xx(p); double zz, Sum2pq=0.0;
+  for(int i=0; i<p; i++){ xx[i] = mean(X(_,i)); }
+  if(Code012){
+    for(int i=0; i<p; i++){ Sum2pq = Sum2pq + xx[i]*xx[i]/2;}
+  }else{
+    for(int i=0; i<p; i++){ Sum2pq = Sum2pq + var(X(_,i));}}
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      if(i<=j ){
+   zz = sum( (X(i,_)-xx(i))*(X(j,_)-xx(j)) );
+   K(i,j)=zz; K(j,i)=zz;}
+    }
+  }
+  return K/Sum2pq;
+}
