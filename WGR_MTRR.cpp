04773ce72a5f48c03d29664226bdeb4eac39305f
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2019-02-05 12:02:52 -0600

    Add files via upload

--- WGR_MTRR.cpp
+++ WGR_MTRR.cpp
@@ -2,7 +2,7 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-SEXP mrr(NumericMatrix Y, NumericMatrix X){
+SEXP MVRR(NumericMatrix Y, NumericMatrix X, bool Choleski = false){
   
   // Convergence parameters
   int maxit = 350;
@@ -10,12 +10,11 @@ SEXP mrr(NumericMatrix Y, NumericMatrix X){
   
   // Obtain environment containing function
   Rcpp::Environment base("package:base");
-  Rcpp::Function chol2inv = base["chol2inv"];
   Rcpp::Function solve = base["solve"];
-  Rcpp::Function diag = base["diag"];
+  Rcpp::Function chol2inv = base["chol2inv"];
   
   // Functions starts here
-  int k = Y.ncol(),p = X.ncol(), n0 = X.nrow();
+  int k = Y.ncol(), p = X.ncol(), n0 = X.nrow();
   
   // Handle missings Y's
   NumericMatrix fit(n0,k),o(n0,k),y(n0,k),e(n0,k);
@@ -25,11 +24,16 @@ SEXP mrr(NumericMatrix Y, NumericMatrix X){
   NumericVector n = colSums(o);
   
   // Marker variance
-  NumericMatrix xx(p,k);
+  NumericMatrix xx(p,k), vx(p,k);
+  double tmp;
   for(int i=0; i<p; i++){
     for(int j=0; j<k; j++){
-    xx(i,j) = sum(X(_,i)*X(_,i)*o(_,j));}}
-  NumericVector MSx = colMeans(xx);
+      xx(i,j) = sum(X(_,i)*X(_,i)*o(_,j));
+      tmp = sum(X(_,i)*o(_,j))/n(j);
+      vx(i,j) = xx(i,j)/n(j)-tmp*tmp;}}
+  
+  //NumericVector MSx = colSums(xx);
+  NumericVector MSx = colSums(vx);
   
   // Beta, intersept and residuals
   NumericMatrix b(p,k),vb(k,k),iG(k,k),rho(k,k),LHS(k,k);
@@ -42,8 +46,8 @@ SEXP mrr(NumericMatrix Y, NumericMatrix X){
     ve(i) = vy(i)*0.5;
     vb(i,i) = ve(i)/MSx(i);
     rho(i,i) = 1;}
-  iG = chol2inv(vb);
-
+  iG = solve(vb);
+  
   // Convergence control
   NumericMatrix bc(p,k);
   int numit = 0;
@@ -59,16 +63,13 @@ SEXP mrr(NumericMatrix Y, NumericMatrix X){
       LHS = iG+0;
       for(int i=0; i<k; i++){
         LHS(i,i) = iG(i,i)+(xx(j,i)/ve(i));
-        RHS(i) = (sum(e(_,i)*X(_,j))+xx(j,i)*b0(i))/ve(i);
-      }
+        RHS(i) = (sum(e(_,i)*X(_,j))+xx(j,i)*b0(i))/ve(i);}
       // Update effects
       b1 = solve(LHS, RHS);
       b(j,_) = b1;
       // Update residuals
       for(int i=0; i<k; i++){
-        e(_,i) = (e(_,i)-X(_,j)*(b1(i)-b0(i)))*o(_,i);
-      }
-    }
+        e(_,i) = (e(_,i)-X(_,j)*(b1(i)-b0(i)))*o(_,i);}}
     
     // Intercept update
     eM = colSums(e)/n;
@@ -78,7 +79,9 @@ SEXP mrr(NumericMatrix Y, NumericMatrix X){
     // Variance components update
     for(int i=0; i<k; i++){
       ve(i) = sum(e(_,i)*y(_,i))/(n(i)-1);
-      vb(i,i) = (1.1*vy(i)-ve(i))/MSx(i);
+      if(Choleski){
+        vb(i,i) = (1.1*vy(i)-ve(i))/MSx(i);
+      }else{ vb(i,i) = (vy(i)-ve(i))/MSx(i);}
     }
     
     // Approximate genetic correlation
@@ -87,19 +90,17 @@ SEXP mrr(NumericMatrix Y, NumericMatrix X){
         fit(i,j) = sum(X(i,_)*b(_,j));}}
     for(int i=0; i<k; i++){ 
       for(int j=0; j<k; j++){
-         rho(i,j) = sum(fit(_,i)*fit(_,j))/sqrt(sum(fit(_,i)*fit(_,i))*sum(fit(_,j)*fit(_,j)));
-          }}
+        rho(i,j) = sum(fit(_,i)*fit(_,j))/sqrt(sum(fit(_,i)*fit(_,i))*sum(fit(_,j)*fit(_,j)));
+      }}
     
     // Covariance components
     for(int i=0; i<k; i++){
       for(int j=0; j<k; j++){
         if(i>j){
           vb(i,j) = rho(i,j)*sqrt(vb(i,i)*vb(j,j));
-          vb(j,i) = vb(i,j);
-        }
-      }}
-    //iG = solve(vb);
-    iG = chol2inv(vb);
+          vb(j,i) = vb(i,j); }}}
+    for(int i=0; i<k; i++){vb(i,i)=vb(i,i)*1.01;}
+    if(Choleski){iG = chol2inv(vb);}else{iG = solve(vb);}
     
     // Convergence
     ++numit;
@@ -114,8 +115,9 @@ SEXP mrr(NumericMatrix Y, NumericMatrix X){
   // Heritability
   NumericVector h2(k); 
   for(int i=0; i<k; i++){ h2 = (vb(i,i)*MSx(i))/((vb(i,i)*MSx(i))+ve); }
-    
+  
   // Output
-  return List::create(Named("mu")=mu, Named("beta")=b,
+  return List::create(Named("mu")=mu, Named("b")=b,
                       Named("hat")=fit, Named("h2")=h2,
-                      Named("Vb")=vb, Named("Ve")=ve);}
+                      Named("Vb")=vb, Named("Ve")=ve,
+                      Named("Vy")=vy, Named("MSx")=MSx);}
