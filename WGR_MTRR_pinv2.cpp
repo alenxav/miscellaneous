83f64057881184e5e29104eaba230a8a033fad1f
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2019-05-15 11:29:08 -0500

    Update WGR_MTRR_pinv2.cpp

--- WGR_MTRR_pinv2.cpp
+++ WGR_MTRR_pinv2.cpp
@@ -76,7 +76,7 @@ SEXP MV(NumericMatrix Y, NumericMatrix X){
     for(int i=0; i<n0; i++){ for(int j=0; j<k; j++){fit(i,j) = sum(X(i,_)*b(_,j));}}
     for(int i=0; i<k; i++){ for(int j=0; j<k; j++){
       vb(i,j) = (sum(fit(_,i)*eps(_,j))+sum(fit(_,j)*eps(_,i)))/((n(i)*MSx(i))+(n(j)*MSx(j)));}}
-    // Generalized inverse of G2A_Kernels
+    // Generalized inverse of G
     G = as<arma::mat>(vb);
     invG = pinv(G,0.00001);
     iG = wrap(invG);
