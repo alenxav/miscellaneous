b367932348ebd197a98cf3b19a03bfc39f9ba2a4
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2019-05-23 15:20:53 -0500

    Update WGR_MTRR_pinv2.cpp

--- WGR_MTRR_pinv2.cpp
+++ WGR_MTRR_pinv2.cpp
@@ -7,7 +7,7 @@ using arma::pinv;
 // [[Rcpp::export]]
 SEXP MV(NumericMatrix Y, NumericMatrix X){
   // Convergence criteria
-  int maxit = 200;
+  int maxit = 250;
   double tol = 10e-8;
   // Obtain environment containing function
   Rcpp::Environment base("package:base");
@@ -73,26 +73,28 @@ SEXP MV(NumericMatrix Y, NumericMatrix X){
     // Residual variance components update
     for(int i=0; i<k; i++){ ve(i) = (sum(e(_,i)*y(_,i)))/n(i);}
     // Genetic covariance components update
-    for(int i=0; i<n0; i++){ for(int j=0; j<k; j++){fit(i,j) = sum(X(i,_)*b(_,j));}}
+    for(int i=0; i<n0; i++){
+      for(int j=0; j<k; j++){fit(i,j)=sum(X(i,_)*b(_,j));}}
     for(int i=0; i<k; i++){ for(int j=0; j<k; j++){
-      vb(i,j) = (sum(fit(_,i)*eps(_,j))+sum(fit(_,j)*eps(_,i)))/((n(i)*MSx(i))+(n(j)*MSx(j)));}}
+      tmp = (sum(fit(_,i)*eps(_,j))+sum(fit(_,j)*eps(_,i)));
+      vb(i,j) = tmp/((n(i)*MSx(i))+(n(j)*MSx(j)));}}
     // Generalized inverse of G
     G = as<arma::mat>(vb);
-    invG = pinv(G,0.00001);
+    invG = pinv(G,0.0001);
     iG = wrap(invG);
     // Convergence
     ++numit;
     cnv = sum(abs(bc-b));
     if( cnv<tol ){break;}}
   // Fitting the model
-  for(int i=0; i<n0; i++){for(int j=0; j<k; j++){fit(i,j)=sum(X(i,_)*b(_,j))+mu(j);}}
-  // Heritability
-  NumericVector h2(k); 
-  for(int i=0; i<k; i++){ h2[i] = (vb(i,i)*MSx[i])/(vb(i,i)*MSx[i]+ve[i]); }
-  // Genetic correlations
+  for(int i=0; i<n0; i++){
+    for(int j=0; j<k; j++){fit(i,j)=sum(X(i,_)*b(_,j))+mu(j);}}
+  // Heritability and genetic correlations
+  NumericVector h2 = 1 - ve/vy;
   NumericMatrix GC(k,k);
-  for(int i=0; i<k; i++){ for(int j=0; j<k; j++){GC(i,j)=vb(i,j)/(sqrt(vb(i,i)*vb(j,j)));}}
-  // Output
+  for(int i=0; i<k; i++){ for(int j=0; j<k; j++){
+    GC(i,j)=vb(i,j)/(sqrt(vb(i,i)*vb(j,j)));}}
+  // Output list
   return List::create(Named("mu")=mu,
                       Named("b")=b,
                       Named("hat")=fit,
