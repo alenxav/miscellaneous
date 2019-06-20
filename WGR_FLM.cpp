3764686de906b9c4d2ea893d60a0c0b040f28e2a
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2019-06-19 21:33:19 -0500

    Update WGR_FLM.cpp

--- WGR_FLM.cpp
+++ WGR_FLM.cpp
@@ -1,61 +1,38 @@
-SEXP FLM(NumericVector y, NumericMatrix gen){
-  // Convergence criteria
-  int maxit = 300;
-  double tol = 10e-6;
-  // Functions starts here
-  int p = gen.ncol();
-  int n = gen.nrow();
-  // Beta, mu and epsilon
-  double b0,eM;
-  double mu = mean(y);
-  NumericVector e = y-mu;
-  // Marker variance
-  NumericVector xx(p);
-  for(int k=0; k<p; k++){xx[k] = sum(gen(_,k)*gen(_,k));}
-  NumericVector vx(p);
-  for(int k=0; k<p; k++){vx[k] = var(gen(_,k));}
-  double cxx = sum(vx);
-  // Regulation coefficients
-  double Ve;
-  NumericVector Vb(p);
-  NumericVector b(p);
-  NumericVector Lmb = p+cxx;
-  double b1;
-  // Convergence control
-  NumericVector bc(p);
-  int numit = 0;
-  double cnv = 1;
-  // Loop
-  while(numit<maxit){
-    // Regression coefficients loop
-    bc = b+0;
-    for(int j=0; j<p; j++){
-      // Ordinary Least Square
-      b0 = b[j];
-      b1 = (sum(gen(_,j)*e)+xx[j]*b0)/(Lmb(j)+xx(j));
-      b[j] = b1;
-      // Residuals update
-      e = e-gen(_,j)*(b1-b0);}
-    // Intercept update
-    eM = mean(e);
-    mu = mu+eM;
-    e = e-eM;
-    // Variance components
-    Ve = sum(e*y)/(n-1);
-    Vb = b*b+(Ve/(xx+Lmb+0.00001));
-    Lmb = sqrt(cxx*Ve/Vb);
-    // Convergence
-    ++numit;
-    cnv = sum(abs(bc-b));
-    if( cnv<tol ){break;}
-  }
-  // Fitting the model
-  NumericVector fit(n); for(int k=0; k<n; k++){ fit[k] = sum(gen(k,_)*b)+mu; }
-  // Output
+#include <Rcpp.h>
+using namespace Rcpp;
+// [[Rcpp::export]]
+SEXP FLM(NumericVector y, NumericMatrix X){
+  // Convergence settings
+  int maxit = 300; double tol = 10e-8;
+  // Initial settings and starting values
+  int p=X.ncol(), n=X.nrow(), numit=0;
+  double b0,b1,eM,Ve,cnv=1,mu=mean(y),Lmb2=0;
+  NumericVector e=y-mu,Vb(p),b(p),fit(n);
+  // Cross-products and shape parameter
+  NumericVector xx(p),sx(p),bc(p);
+  for(int k=0; k<p; k++){
+    xx[k]=sum(X(_,k)*X(_,k));
+    if(xx[k]==0) xx[k]=0.1;
+    Lmb2=Lmb2+var(X(_,k));}
+  NumericVector iTau2=p+Lmb2;
+  // Looping across parameters until convergence
+  while(numit<maxit){   
+    // Updating markers effects
+    bc=b+0; for(int j=0; j<p; j++){ b0=b[j];
+      b1=(sum(X(_,j)*e)+xx[j]*b0)/(iTau2(j)+xx(j));
+      b[j]=b1; e=e-X(_,j)*(b1-b0);}
+    // Updating intercept
+    eM=mean(e); mu=mu+eM; e=e-eM;
+    // Updating variance components
+    Ve=sum(e*y)/(n-1); 
+    Vb=b*b+Ve/(xx+iTau2);
+    iTau2=sqrt(Lmb2*Ve/Vb);
+    // Check parameters convergence
+    ++numit; cnv=sum(abs(bc-b));
+    if(cnv<tol){break;}}
+  // Fit model
+  for(int k=0; k<n; k++){fit[k]=mu+sum(X(k,_)*b);}
+  // Return output
   return List::create(Named("mu")=mu,
-                      Named("b")=b,
-                      Named("hat")=fit,
-                      Named("Vb")=Vb,
-                      Named("Ve")=Ve,
-                      Named("Lmb")=cxx);
-}
+                      Named("b")=b, Named("fit")=fit,
+                      Named("T2")=1/iTau2, Named("Ve")=Ve);}
