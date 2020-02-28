fbc3f7467cdbbc512ca82cd3cb17238e7e141f97
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2020-02-28 12:41:55 -0600

    Add files via upload

--- Play_FNN.R
+++ Play_FNN.R
@@ -1,5 +1,5 @@
-require(NAM)
 if(!exists('Y')){
+  require(NAM)
   tmp = SoyNAM::BLUP(family=1:5)
   Y = tmp$Phen
   Z = CNT(tmp$Gen)
@@ -10,15 +10,12 @@ if(!exists('Y')){
 
 # Gauss-Seidel Gradients
 gsg = function(Y,X,h2=1){
+  X = CNT(X)
   xx = apply(X,2,crossprod)+1e-8
   lmb = mean(xx)*(1-h2)/h2
-  #gs = function(Y) NAM::NOR(Y,X,lmb,xx,1)$b
-  gs = function(Y){
-    h = NAM::NOR(Y,X,lmb,xx,1)
-    cat(h$v,'\n')
-    return(h$b) } 
-  w = apply(Y,2,gs)
-  return(w)}
+  gs = function(Y) NAM::NOR(Y,X,lmb,xx,5)$b
+  coefs = apply(Y,2,gs)
+  return(coefs)}
 
 n = nrow(Z)
 p = ncol(Z)
@@ -33,7 +30,7 @@ ActFun = function(x){ x }  # linear
 # Fit node
 FN = function(Z,W,I){
   tmp = Z %*% W
-  tmp = t(apply(tmp,1,function(x)x+I))
+  for(k in 1:length(I)) tmp[,k] = tmp[,k]+I[k]
   tmp = ActFun(tmp)
   return(tmp)
 }
@@ -57,64 +54,62 @@ W3 = rnorm(HL2)
 H3 = c(FN(H2,W3,I3))
 
 # Check starting point
-CHECK = function(){
+CHECK = function(a='check'){
   Hat = c(FN(FN(FN(Z,W1,I1),W2,I2),W3,I3))
-  par(mfrow=c(1,1)); plot(Hat,Y,main=paste("COR =",round(cor(Hat,Y),4)))}
+  plot(Hat,Y,main=paste("COR =",round(cor(Hat,Y),4),a))}
 CHECK()
 
 # Learning rate and L2 penalization
 rates = c(0.25,0.5,1.0)
 lmb = c(0.01,0.01,0.01)
 
+########################### THIRD ATTEMPT
 
-########################### SECOND ATTEMPT
+par(mfrow=c(2,2))
+CHECK()
 
 if(T){
   
-  for(iter in 1:8){
+  for(iter in 1:3){
     cat('\n ITERATION',iter,'\n')
     
     # Batch
-    BatchSize = 0.5
+    BatchSize = 0.75
     mb = sort(sample(1:n,n*BatchSize)) # mini batch
     
     # Fit model
-    H1 = FN(Z[mb,],W1,I1)
-    H2 = FN(H1,W2,I2)
-    Hat = c(FN(H2,W3,I3))
     
-    # Layer 3
-    # Backprop
+    # Layer 1 backprop
+    Hat = c(FN(FN(FN(Z[mb,],W1,I1),W2,I2),W3,I3))
     dH3 = matrix(Y[mb]-Hat)
-    #dW3 = t(H2)%*%dH3
-    dW3 = gsg(dH3,H2)
-    dI3 = mean(dH3)
-    # Update parameter 
-    W3 = W3 + c(dW3)*rates[2] #- (dW3+lmb[3])*rates[3]
-    I3 = I3 - dI3*rates[3]
-    CHECK()
-    
-    # Layer 2
-    # Backprop
     dH2 = ActFun( dH3 %*% t(W3) )
-    #dW2 = t(H1)%*%dH2
-    dW2 = gsg(dH2,H1,h2=0.5)
-    dI2 = colMeans(dH2)
-    # Update parameter 
-    W2 = W2 + dW2*rates[2] #- (dW2+lmb[2])*rates[2]
-    I2 = I2 - dI2*rates[2]
-    CHECK()
-    
-    # Layer 1
-    # Backprop
-    dH1 = ActFun( dH2 %*% t(W2) ) # y
-    #dW1 = t(Z[mb,])%*%dH1 # rhs
-    dW1 = gsg(dH1,Z[mb,],h2=0.5)
+    dH1 = ActFun( dH2 %*% t(W2) )
+    dW1 = gsg(dH1,Z[mb,],h2=0.25)
     dI1 = colMeans(dH1)
-    # Update parameter 
-    W1 = W1 + dW1*rates[1] #- (dW1+lmb[1])*rates[1]
-    I1 = I1 - dI1*rates[1]
-    CHECK()
+    W1 = W1 + dW1*rates[1]
+    I1 = I1 + dI1*rates[1]
+    CHECK(1)
+    
+    # Layer 2 backprop
+    Hat = c(FN(FN(FN(Z[mb,],W1,I1),W2,I2),W3,I3))
+    dH3 = matrix(Y[mb]-Hat)
+    dH2 = ActFun( dH3 %*% t(W3) )
+    dW2 = gsg(dH2,H1,h2=0.25)
+    dI2 = colMeans(dH2)
+    W2 = W2 + dW2*rates[2]
+    I2 = I2 - dI2*rates[2]
+    CHECK(2)
+    
+    # Layer 3 backprop
+    Hat = c(FN(FN(FN(Z[mb,],W1,I1),W2,I2),W3,I3))
+    dH3 = matrix(Y[mb]-Hat)
+    dW3 = gsg(dH3,H2,h2=1)
+    dI3 = mean(dH3)
+    W3 = W3 + c(dW3)*rates[2]
+    I3 = I3 - dI3*rates[3]
+    CHECK(3)
+    
+    
     
     
   }
