3bcc61c47536de920fd945ca62ca3b134b2576d1
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2020-03-27 09:26:04 -0500

    Update Play_GradDesc.R

--- Play_GradDesc.R
+++ Play_GradDesc.R
@@ -11,12 +11,16 @@ e = y-Zb
 a = 1/ncol(Z)
 plot(Zb,y)
 
-for(i in 1:50){
+par(mfrow=c(3,3),mar=c(1,1,2,1),cex=0.8)
+for(i in 1:45){
   e = y-Zb
   d = c(-2*crossprod(e,Z)+2*lmb*b)/length(y)
   b = b - d*a
   Zb = c(Z%*%b)
   cat('Cor',round(cor(Zb,h$hat),4),'\n')
+  if(i %% 5 == 0){
+    p = paste('Cor',round(cor(Zb,h$hat),8),'\n')
+    plot(b,h$b,pch=20,main=paste('Iteration',i),xaxt='n',yaxt='n')
+    legend('topleft',legend = p,bty='n')
+  }
 }
-
-plot(b)
