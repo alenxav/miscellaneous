efe58352e0374b8dade2ee77d2160acf2c5d54bb
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2020-06-05 13:38:35 -0500

    Update WGR_EmpLasso.cpp

--- WGR_EmpLasso.cpp
+++ WGR_EmpLasso.cpp
@@ -35,6 +35,7 @@ SEXP Lasso(NumericVector y, NumericMatrix X){
       e = e-X(_,j)*b[j];}
     // Update regularization and intercept
     Lmb = 2*mean(abs(yx)-abs(b*xx));
+    Lmb = 2*sqrt(Lmb); // this line was added by ad hoc, without derivations
     eM = mean(e);
     mu = mu+eM;
     e = e-eM;
