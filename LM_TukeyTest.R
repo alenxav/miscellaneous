b092b6f2d326f5d1ded93e1f9c3e4f7795c007d1
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2020-09-18 12:45:28 -0500

    Update LM_TukeyTest.R

--- LM_TukeyTest.R
+++ LM_TukeyTest.R
@@ -1,32 +1,17 @@
-tlm = function(formula,data,Tukey=TRUE){
+tlm = function(formula,data,Tukey=F,...){
   
   # fit the model
   fnew = update(formula,~.-1)
   fit = lm(formula=fnew,data=data)
-  
-  # recreate formula
+  Y = predict(fit,data,terms=key,type='term')+fit$residuals
   data_new = fit$model
-  nl = length(levels(data_new[[2]]))
-  fit$terms = formula(terms.formula(formula)[-1])
-  fit$coefficients = fit$coefficients[-c(1:nl)]
-  fit$effects = fit$effects[-c(1:nl)]
-  fit$rank = fit$rank-nl
-  fit$assign = fit$assign[-c(1:nl)]-1
-  fit$xlevels = fit$xlevels[-1]
-  if(length(fit$contrasts)>1){
-    fit$contrasts = fit$contrasts[-1]
-    }else{fit$contrasts=NULL}
-  
-  # Rearrange data with conditional phenotype
-  suppressWarnings(hat <- predict(fit,data_new))
-  y = data_new[as.character(formula[2])][,1]
-  res = (y - hat);
-  Y = res-mean(res)+mean(y)
-  X = data_new[as.character(formula( terms.formula(formula)[1] ))[3]][,1]
+  data_new[['Y']] = Y
+  X = data_new[key][,1]
   lvl = levels(X)
   
   # Refit and multiple testing
   refit = lm(Y~X-1,data_new)
+  cat('overall p-value',c(anova(lm(Y~X-1,data_new))["Pr(>F)"])[[1]][1],'\n')
   tk = TukeyHSD(aov(refit))
   tt = pairwise.t.test(Y,X,pool.sd=FALSE)
   
@@ -42,7 +27,7 @@ tlm = function(formula,data,Tukey=TRUE){
   ttX = matrix(NA,nl,nl,dimnames = list(rnk,rnk))
   for(i in lvl){for(j in lvl){
     if(i%in%rownames(tt$p.value) & j%in%colnames(tt$p.value)){
-        ttX[i,j] = ttX[j,i] = tt$p.value[i,j] }}} 
+      ttX[i,j] = ttX[j,i] = tt$p.value[i,j] }}} 
   diag(ttX) = 1
   #
   ttrnk = rep(NA,nl); ttrnk[1] = 1
@@ -55,7 +40,7 @@ tlm = function(formula,data,Tukey=TRUE){
   tkX = matrix(NA,nl,nl,dimnames = list(rnk,rnk))
   for(i in lvl){for(j in lvl){
     if(paste(i,j,sep='-')%in%rownames(tk$X)){
-    tkX[i,j] = tkX[j,i] = tk$X[which(paste(i,j,sep='-')==rownames(tk$X)),4]
+      tkX[i,j] = tkX[j,i] = tk$X[which(paste(i,j,sep='-')==rownames(tk$X)),4]
     }}} 
   diag(tkX) = 1
   #
@@ -68,9 +53,9 @@ tlm = function(formula,data,Tukey=TRUE){
   
   # bar plot
   if(Tukey){out = tkrnk}else{out = ttrnk}
-  barplot(mu, ylim = c(0,max(upr)),space=F,main='Bar plot with SD and class',col=COL)
+  barplot(mu,..., ylim = c(min(lwr)*0.8,max(upr)*1.2),space=F,col=COL,xpd = F)
   for(i in 1:nl) lines(c(i-.5,i-.5),c(lwr[i],upr[i]),lwd=2)
-  for(i in 1:nl) text(i-.35,mu[i],out[lvl][i],cex=1.25)
+  for(i in 1:nl) text(i-.2,(mu[i]*0.3+upr[i]*0.7),out[lvl][i],cex=1.25)
   
   # Return output
   return(out)
