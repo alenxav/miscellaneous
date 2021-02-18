8ecff6f7d685d293894eb29b74bbbef0c258596e
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2021-02-18 16:38:13 -0600

    Update Play_Small_DNN.R

--- Play_Small_DNN.R
+++ Play_Small_DNN.R
@@ -1,13 +1,12 @@
-#ActFun = function(x){x[x<0]=0;return(x)} # RELU
-ActFun = function(x){x[x<0]=x[x<0]*0.25;return(x)} # Leaky ReLU
-#ActFun = function(x) x # Linear
-
 # Data
 require(bWGR)
 data(tpod)
-# Centralize
 y = matrix(y-mean(y))
 X = CNT(gen)
+
+# DNN functions
+ActFun = function(x,leaky=0.25){x[x<0]=x[x<0]*leaky;return(x)}
+DropOut = function(x,prc=0.0){x[sample(length(x),length(x)*prc)];return(x)}
 # Dimensions
 n = nrow(X)
 p = ncol(X)
@@ -33,15 +32,15 @@ for(i in 1:100){
   e2 = ActFun(e3 %*% t(b3))
   e1 = ActFun(e2 %*% t(b2))
   # Update coefficients
-  b1 = b1 - ( 2*t(X)%*%(H1-e1)*0.0001  )
-  b2 = b2 - ( 2*t(H1)%*%(H2-e2)*0.01  )
-  b3 = b3 - ( 2*t(H2)%*%(H3-e3)*0.1  )
-  # Print Residual Variance
+  b1 = b1 - DropOut(2*t(X)%*%(H1-e1)*0.0001)
+  b2 = b2 - DropOut(2*t(H1)%*%(H2-e2)*0.01 )
+  b3 = b3 - DropOut(2*t(H2)%*%(H3-e3)*0.1  )
+  # Store convergence
   CNV1 = c(CNV1,mean(apply(e3,2,var,na.rm=T)))
   CNV2 = c(CNV2,mean(diag(cor(H3,y,use='p'))))
 }
 
-
+# Plot convergence
 par(mfrow=c(1,2))
 plot(CNV1,ylab='MSE',type='l')
 plot(CNV2,ylab='COR',type='l')
