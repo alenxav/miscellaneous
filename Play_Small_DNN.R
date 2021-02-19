904af36608f7b552de1f80d4ee08b78d91d2de6f
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2021-02-19 13:02:46 -0600

    Update Play_Small_DNN.R

--- Play_Small_DNN.R
+++ Play_Small_DNN.R
@@ -1,7 +1,7 @@
 # Data
 require(bWGR)
 data(tpod)
-y = matrix(y-mean(y))
+y = matrix(scale(y))
 X = CNT(gen)
 
 # DNN functions
@@ -11,9 +11,8 @@ DropOut = function(x,prc=0.0){x[sample(length(x),length(x)*prc)];return(x)}
 n = nrow(X)
 p = ncol(X)
 k = ncol(y)
-# Number of nodes
-n1 = 20
-n2 = 20
+# Learning settings
+n1 = 8; n2 = 4; rate = 0.01; lmb = 10
 # Starting weights
 b1 = matrix(rnorm(n1*p,0,1/p),p,n1)
 b2 = matrix(rnorm(n1*n2,0,1/n1),n1,n2)
@@ -27,14 +26,13 @@ for(i in 1:100){
   H2 = ActFun(H1%*%b2)
   H3 = H2%*%b3
   # Gradients
-  e3 = y-H3
-  if(anyNA(e3)) e3[is.na(e3)]=0
+  e3 = y-H3; if(anyNA(e3)) e3[is.na(e3)]=0
   e2 = ActFun(e3 %*% t(b3))
   e1 = ActFun(e2 %*% t(b2))
   # Update coefficients
-  b1 = b1 - DropOut(t(X)%*%(H1-e1)+b1)*0.0001
-  b2 = b2 - DropOut(t(H1)%*%(H2-e2)+b2)*0.01
-  b3 = b3 - DropOut(t(H2)%*%(H3-e3)+b3)*0.1
+  b1 = b1 - DropOut( t(X)%*%(H1-e1)+lmb*b1)*(2/n)*rate
+  b2 = b2 - DropOut(t(H1)%*%(H2-e2)+lmb*b2)*(2/n)*rate
+  b3 = b3 - DropOut(t(H2)%*%(H3-e3))*(2/n)*rate
   # Store convergence
   CNV1 = c(CNV1,mean(apply(e3,2,var,na.rm=T)))
   CNV2 = c(CNV2,mean(diag(cor(H3,y,use='p'))))
