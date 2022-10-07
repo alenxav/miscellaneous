42c77032d3c9b47af485785025faf3520e638ad4
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2022-10-07 10:32:21 -0500

    Update AccByC.R

--- AccByC.R
+++ AccByC.R
@@ -5,9 +5,9 @@ X2 = gen[101:196,]
 
 Acc2 = function(X1,X2,h2=0.4){
   alpha = 1/sqrt(mean(apply(X1,1,crossprod)))
-  V = EigenBDCSVD(crossprod(X1)/alpha,T)$V
-  Z1 = X1 %*% V *alpha
-  Z2 = X2 %*% V *alpha
+  V = svd(crossprod(X1),T)$v *alpha
+  Z1 = X1 %*% V 
+  Z2 = X2 %*% V 
   D = apply(Z1,2,crossprod)
   ve = ((1-h2)/h2)
   VarBhat = 1-1/(D/ve+1)
@@ -16,6 +16,7 @@ Acc2 = function(X1,X2,h2=0.4){
   return(Acc)}
 
 h2=0.5
-tmp = EigenAcc(X1,X2,h2)
-a = Acc2(X1,X2,h2)
-plot(a,tmp)
+baseline = EigenAcc(X1,X2,h2)
+AccByC = Acc2(X1,X2,h2)
+plot(AccByC,baseline)
+
