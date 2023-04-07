71f8893ccbc345977ccd40549f317f61519ff7d9
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2023-04-06 19:42:53 -0500

    Update GRM_GaussianKernel.cpp

--- GRM_GaussianKernel.cpp
+++ GRM_GaussianKernel.cpp
@@ -1,9 +1,15 @@
-#include <Rcpp.h>
-using namespace Rcpp;
+// [[Rcpp::plugins(openmp)]]
+// [[Rcpp::depends(RcppEigen)]]
+#include <RcppEigen.h>
+
 // [[Rcpp::export]]
-NumericMatrix GAU(NumericMatrix X){
-  int n = X.nrow(); NumericVector D; NumericMatrix K(n,n); double d2, md;
-  for(int i=0; i<n; i++){; for(int j=0; j<n; j++){
-  if(i==j){ K(i,j)=0; }else if(j>i){; D = X(i,_)-X(j,_);
-  d2 = sum(D*D); K(i,j)=d2; K(j,i)=d2; }}}; md = mean(K);
-  for(int i=0; i<n; i++){K(i,_) = exp(-K(i,_)/md);} return K;}
+Eigen::MatrixXd GAU(Eigen::MatrixXd X, double phi = 1.0, int cores = 2){
+  Eigen::setNbThreads(cores);
+  int n = X.rows(); double tmp;
+  Eigen::MatrixXd XXp = X*X.transpose();
+  for(int i=0; i<n; i++){ for(int j=0; j<n; j++){ if(i>j){
+    tmp = sqrt(XXp(i,i) + XXp(j,j) - 2*XXp(i,j));
+    XXp(i,j) = tmp*1.0; XXp(j,i) = tmp*1.0;}}};
+  for(int i=0; i<n; i++){XXp(i,i) = 0.0;}
+  tmp = phi * (-n*(n-1)) / (XXp.colwise().sum()).sum();
+  XXp *= tmp; return exp(XXp.array());}
