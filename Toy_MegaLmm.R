187c357b4d95fc5550c95f64241dc5931114bdcb
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2024-05-19 19:21:21 -0500

    Update Toy_MegaLmm.R

--- Toy_MegaLmm.R
+++ Toy_MegaLmm.R
@@ -1,5 +1,5 @@
 # Preconditioning on FA
-MEGA_PCFA = function(Y,Z,npc=3){
+MEGA_PCFA = function(Y,Z,npc=2*sqrt(ncol(Y))){
   mu0 = colMeans(Y,na.rm=T)
   sd0 = apply(Y,2,sd,na.rm=T)
   for(i in 1:ncol(Y)) Y[,i]= (Y[,i]-mu0[i])/sd0[i]
@@ -7,18 +7,15 @@ MEGA_PCFA = function(Y,Z,npc=3){
   Y2 = Y; Y2[is.na(Y2)] = Yh$hat[is.na(Y2)]
   E = bWGR::EigenBDCSVD(Y2)
   FA = E$U[,1:npc] %*% diag(E$D[1:npc]) %*% t(E$V[,1:npc])
-  FAh = bWGR:::ZSEMF(FA,Z)
+  FAh = bWGR:::ZSEMF(E$U[,1:npc],Z)
   Jh = bWGR:::ZSEMF(Y-FA,Z)
-  mu = FAh$mu + Jh$mu + mu0
-  b = t(sd0 * t(FAh$b + Jh$b))
+  b = t(sd0 * t( FAh$b %*% diag(E$D[1:npc]) %*% t(E$V[,1:npc])  + Jh$b))
   gebv = Z %*% b
   hat = t(mu0 + sd0 * t(FA + Jh$hat))
-  h2 = FAh$h2 + Jh$h2
+  h2 = sapply(1:ncol(Y),function(i) cor(Y[,i],gebv[,i],use='p')^2 )
   colnames(gebv) = names(h2) = names(mu) = colnames(Y)
   colnames(b) = colnames(Y); rownames(b) = colnames(Z)
-  GC = cor(gebv)
-  return(list(mu=mu, b=b, gebv=gebv, hat=hat, GC=GC, h2=h2))
-}
+  return(list(mu=mu, b=b, gebv=gebv, hat=hat, GC=cor(gebv), h2=h2))}
 
 # Simulate some data
 Z = bWGR::SimZ()
